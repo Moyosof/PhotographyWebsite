f7fc433e1babb892b1a9037d08073e4622f5bfbf
AlimiSamuel <AlimiSamuel121@gmail.com>
2023-05-10 09:57:24 +0100

    tailwind commit

--- PhotoSite/src/App.jsx
+++ PhotoSite/src/App.jsx
@@ -2,20 +2,22 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Photoshoot from './pages/photoshoot/Photoshoot'
+import Navbar from './component/navbar/Navbar'
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-      <Routes>
-        <Route path='/'>
-          <Route index element = {<Home/>} />
-          <Route path='/photography' element = {<Photoshoot/>} />
-        </Route>
-      </Routes>
+        <Navbar />
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="/photography" element={<Photoshoot />} />
+          </Route>
+        </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
 export default App
